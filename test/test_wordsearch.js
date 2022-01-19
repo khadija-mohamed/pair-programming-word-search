0f818b8cd981c4264106c53cf254f62ddef7e80e
Khadija Mohamed <khadijam@live.ca>
2022-01-19 18:56:29 -0500

    Pair-Programming: Word Search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,36 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return false if there is no word", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], ' ')
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'H', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'E', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'L', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'L', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'O', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HELLO')
+
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let array = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      array.push([]);
     }
-}
-
-module.exports = wordSearch
+    for (let row = 0; row < matrix.length; row++) {
+      for (let col = 0; col < matrix[row].length; col++) {
+        array[col][row] = matrix[row][col];
+      }
+    }
+    return array;
+  };
+  const wordSearch = (letters, word) => {
+    const verticalJoin = transpose(letters);
+    let verticalJoinArray = [];
+    verticalJoin.forEach(item => {
+      let verticalWord = "";
+      item.forEach(letter => {
+        verticalWord += letter;
+      });
+      verticalJoinArray.push(verticalWord);
+    });
+    for (l of verticalJoinArray) {
+      if (l.includes(word)) return true;
+    }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin); {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
